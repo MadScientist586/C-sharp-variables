bdff30d438cbc9890175529a89b0ec78a38106d3
MadScientist586 <mr.scientist.knight@gmail.com>
2022-09-12 12:22:06 -0500

    first commit

---
+++ C Sharp Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+using System.Xml.Linq;
+
+string firstName = "Aiden";
+int age = 13;
+char middleInitial = 'W';
+bool hasDriversLicence = false;
+double cardNumber = 1234123412341234;
+decimal bankAccountBalance = 123456789.99m;
+
+
+Console.WriteLine($"My name is: {firstName}\n" +
+                  $"My age is: {age}\n" +
+                  $"My middle initial is: {middleInitial}\n" +
+                  $"Drivers licence authentication: {hasDriversLicence}\n" +
+                  $"My card number is: {cardNumber}\n" +
+                  $"My bank account balance is: {bankAccountBalance}\n");
+
+
+
+
